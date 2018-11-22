a6b85aab0d3d3f2f73f2c5f49eb4ad095a58957f
onemole <olusegun@summitech.ng>
2018-11-22 10:12:47 +0100

    fixed android and ios link bug in view project

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,64 +9,37 @@ class Project extends React.Component {
   }
   render() {
     if (isMobile) {
-      if (isAndroid) {
-        return (
-          <div className="section">
-            <div className={`project ${this.props.right ? "project__right" : ""}`}>
-              <div className="row">
-                <div className="col-md-12 col-lg-4">
-                  <div className="text-block">
-                    <h5 className="project-title">{this.props.name}</h5>
-                    <p className="project-category">{this.props.category}</p>
-                    <p className="project-description">{this.props.description}</p>
-                    <a href={this.props.url} className="button button__secondary" title={`${this.props.name} link`}>
-                      <span className="button-text">view project</span>
-                      <div className="button-mask" />
-                      <span className="button-icon">
-                        <i className="fas fa-long-arrow-alt-right" />
-                      </span>
-                    </a>
-                  </div>
+      return (
+        <div className="section">
+          <div className={`project ${this.props.right ? "project__right" : ""}`}>
+            <div className="row">
+              <div className="col-md-12 col-lg-4">
+                <div className="text-block">
+                  <h5 className="project-title">{this.props.name}</h5>
+                  <p className="project-category">{this.props.category}</p>
+                  <p className="project-description">{this.props.description}</p>
+                  <a
+                    href={isIOS && this.isEmpty(this.props.altUrl) ? this.props.altUrl : this.props.url}
+                    className="button button__secondary"
+                    title={`${this.props.name} link`}
+                  >
+                    <span className="button-text">view project</span>
+                    <div className="button-mask" />
+                    <span className="button-icon">
+                      <i className="fas fa-long-arrow-alt-right" />
+                    </span>
+                  </a>
                 </div>
-                <div className="col-md-12 col-lg-8">
-                  <div className="image">
-                    <img src={this.props.image || project1} className="img-fluid" alt={this.props.imageAlt} />
-                  </div>
+              </div>
+              <div className="col-md-12 col-lg-8">
+                <div className="image">
+                  <img src={this.props.image || project1} className="img-fluid" alt={this.props.imageAlt} />
                 </div>
               </div>
             </div>
           </div>
-        );
-      }
-      if (isIOS) {
-        return (
-          <div className="section">
-            <div className={`project ${this.props.right ? "project__right" : ""}`}>
-              <div className="row">
-                <div className="col-md-12 col-lg-4">
-                  <div className="text-block">
-                    <h5 className="project-title">{this.props.name}</h5>
-                    <p className="project-category">{this.props.category}</p>
-                    <p className="project-description">{this.props.description}</p>
-                    <a href={this.props.altUrl} className="button button__secondary" title={`${this.props.name} link`}>
-                      <span className="button-text">view project</span>
-                      <div className="button-mask" />
-                      <span className="button-icon">
-                        <i className="fas fa-long-arrow-alt-right" />
-                      </span>
-                    </a>
-                  </div>
-                </div>
-                <div className="col-md-12 col-lg-8">
-                  <div className="image">
-                    <img src={this.props.image || project1} className="img-fluid" alt={this.props.imageAlt} />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      }
+        </div>
+      );
     } else {
       if (!this.isEmpty(this.props.url) && !this.isEmpty(this.props.altUrl)) {
         return (
